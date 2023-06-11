efb215f9e184bbe3da29573cfcca75e2830a73e5
Ayodele Gift <135742123+OsGift@users.noreply.github.com>
2023-06-11 10:06:23 +0100

    Updates on Home Page

---
+++ ayodelegift/Controllers/Home2Controller.cs
@@ -0,0 +1,37 @@
+﻿using ayodelegift.Areas.Identity.Data;
+using ayodelegift.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ayodelegift.Controllers
+{
+    public class Home2Controller : Controller
+    {
+        private readonly ILogger<Home2Controller> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public Home2Controller(ILogger<Home2Controller> logger, UserManager<ApplicationUser> userManager)
+        {
+            _logger = logger;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
